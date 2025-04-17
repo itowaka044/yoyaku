1c95954f717ec2926317b5b3ad7481df650fe9a1
joao vitor wakamori <joao.waka@gmail.com>
2025-04-17 08:17:57 -0300

    spring setup

---
+++ src/test/java/com/yoyaku/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoyaku.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
